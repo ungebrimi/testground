cdd03f896f54befc0e36d6c43f17f091e7c3ddb2
Joakim pedersen <devjoakimpedersen@gmail.com>
2022-12-01 10:12:03 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,56 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <div className="sketchfab-embed-wrapper">
-        {" "}
-        <iframe
-          title="my animated horse"
-          frameborder="0"
-          allowfullscreen
-          mozallowfullscreen="true"
-          webkitallowfullscreen="true"
-          allow="autoplay; fullscreen; xr-spatial-tracking"
-          xr-spatial-tracking
-          execution-while-out-of-viewport
-          execution-while-not-rendered
-          web-share
-          src="https://sketchfab.com/models/542a52062aae4101b8f2563a9ebf0355/embed"
-        >
-          {" "}
-        </iframe>{" "}
+      <div id="card">
+        <model-viewer
+          src="https://storage.googleapis.com/virtualia_shop/ar_models/France/Occitanie/Montauban/atelierchezsoi/atelierchezsoi/renard.glb"
+          ios-src="https://storage.googleapis.com/virtualia_shop/ar_models/France/Occitanie/Montauban/atelierchezsoi/atelierchezsoi/renard-poly.usdz"
+          poster=""
+          alt="A 3D model of a fox"
+          shadow-intensity="1"
+          camera-controls=""
+          auto-rotate=""
+          ar=""
+        ></model-viewer>
+        <section classNAme="attribution">
+          <span>
+            <h2 style={{ textAlign: "center" }}>
+              À voir en réalité augmentée sans modération
+            </h2>
+            <span>
+              Credit:{" "}
+              <a href="https://modelviewer.dev/" target="_blank" rel="noopener">
+                Poly
+              </a>
+            </span>
+            <a
+              className="cc"
+              href="https://creativecommons.org/licenses/by/2.0/"
+              target="_blank"
+              rel="noopener"
+            >
+              <img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" />
+              <img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" />
+            </a>
+          </span>
+        </section>
       </div>
+
+      <footer id="footerAR">
+        <span>
+          This model is based on the{" "}
+          <a
+            href="https://github.com/GoogleWebComponents/model-viewer"
+            target="_blank"
+            rel="noopener"
+          >
+            &lt;model-viewer&gt;
+          </a>{" "}
+          web component.
+        </span>
+      </footer>
     </div>
   );
 }
